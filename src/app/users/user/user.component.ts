78e77a8d792178261f366472a3fede51d52b7eb8
David Shaevel <david@davidshaevel.com>
2017-05-17 14:24:51 -0500

    CHECKPOINT 5: query parameters and fragments

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-  paramsSubscription: Subscription
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
