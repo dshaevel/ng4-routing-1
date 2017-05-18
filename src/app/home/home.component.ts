a11d06428e3d2cf7d741f18fd2cae33d33f4e824
David Shaevel <david@davidshaevel.com>
2017-05-18 14:20:29 -0500

    CHECKPOINT 12: using a fake auth service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,12 +10,20 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
 
   onLoadServer(id: number) {
     // complex calculation
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
